a00533a34fad6ecae9969441cce4d5ff448571b2
alefagundes <alefagundes2000.af@gmail.com>
2021-11-30 00:36:45 -0300

    Exception handling - delete

--- src/main/java/com/alessandrof/workingSpring/services/UserService.java
+++ src/main/java/com/alessandrof/workingSpring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.alessandrof.workingSpring.entities.User;
 import com.alessandrof.workingSpring.repositories.UserRepository;
+import com.alessandrof.workingSpring.services.exceptions.DataBaseException;
 import com.alessandrof.workingSpring.services.exceptions.ResourceNotFoundException;
 
 
@@ -32,7 +35,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+		
 	}
 	
 	
